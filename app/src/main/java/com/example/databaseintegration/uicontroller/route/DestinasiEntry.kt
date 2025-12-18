18d36b4a17ebb533c908f52e45d691dc98803528
Pradipa Y <pdpyyo@gmail.com>
2025-12-18 19:42:43 +0700

    fix objek di destinasi entry

--- app/src/main/java/com/example/databaseintegration/uicontroller/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/databaseintegration/uicontroller/route/DestinasiEntry.kt
@@ -2,7 +2,7 @@ package com.example.databaseintegration.uicontroller.route
 
 import com.example.databaseintegration.R
 
-object DestinasiEntry : DestinasiEntry {
+object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
 }
