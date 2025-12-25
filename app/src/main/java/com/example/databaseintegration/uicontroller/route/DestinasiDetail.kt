56fc214146cefb2c527be4fc472b20e9e57cbb63
Pradipa Y <pdpyyo@gmail.com>
2025-12-25 19:43:12 +0700

    menambahkan route DestinasiDetail.kt

--- app/src/main/java/com/example/databaseintegration/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/databaseintegration/uicontroller/route/DestinasiDetail.kt
@@ -1,2 +1,10 @@
 package com.example.databaseintegration.uicontroller.route
 
+import com.example.databaseintegration.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
