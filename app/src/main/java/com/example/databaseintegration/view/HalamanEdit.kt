0944c8f7fe3fbab15221e668a45ee9903774c76d
Pradipa Y <pdpyyo@gmail.com>
2025-12-25 20:52:44 +0700

    membuat editSiswaScreen di HalamanEdit.kt

--- app/src/main/java/com/example/databaseintegration/view/HalamanEdit.kt
+++ app/src/main/java/com/example/databaseintegration/view/HalamanEdit.kt
@@ -21,27 +21,5 @@ fun EditSiswaScreen(
     modifier: Modifier = Modifier,
     viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
-    Scaffold(
-        topBar = {
-            SiswaTopAppBar(
-                title = stringResource(DestinasiEdit.titleRes),
-                canNavigateBack = true,
-                navigateUp = onNavigateUp
-            )
-        },
-        modifier = modifier
-    ) { innerPadding ->
-        val coroutineScope = rememberCoroutineScope()
-        EntrySiswaBody(
-            uiStateSiswa = viewModel.uiStateSiswa,
-            onSiswaValueChange = viewModel::updateUiState,
-            onSaveClick = {
-                coroutineScope.launch {
-                    viewModel.editSatuSiswa()
-                    navigateBack()
-                }
-            },
-            modifier = Modifier.padding(innerPadding)
-        )
-    }
+
 }
